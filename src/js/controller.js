05f6df51e2e0e54c6077c6cb5ffeddbe367d261a
Touhidujjaman-Emon <tj.emon33@gmail.com>
2024-10-04 10:31:24 +0600

    added a spinner

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,7 +24,7 @@ const renderSpinner = function (parentEl) {
           </svg>
         </div> 
   `;
-
+  recipeContainer.innerHTML = '';
   parentEl.insertAdjacentHTML('afterbegin', markup);
 };
 
