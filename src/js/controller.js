cf42ad6faf0b42b1c3fbbdbe7901942ecd9e3d44
emon <tj.emon33@gmail.com>
2024-10-17 20:43:33 +0600

    rendered search with error

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,20 @@
 
-import * as model from './model.js';
-import recipeView from './views/recipeView.js';
-
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
 
 ///////////////////////////////////////
+
+// Hot reload using parcel
+if (module.hot) {
+  module.hot.accept()
+}
 
 const controlRecipe = async function () {
   try {
@@ -31,12 +38,18 @@ const controlRecipe = async function () {
 
 const controlSearchResult = async function() {
   try { 
+     
+    resultsView.renderSpinner()
 
+    // Get search query
     const query = searchView.getQuery();
     if (!query) return;
-
+    
+    // Load search results
     await model.loadSearchResults(query)
-    console.log(model.state.search.results);
+
+    // Render results
+    resultsView.render(model.state.search.results)
 
   } catch (err) {
     console.log(err);
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,33 @@
+//import icons from '../img/icons.svg' // for parcel v1
+import icons from 'url:../../img/icons.svg'; // for parcel v2
+
+import View from './view.js';
+
+class ResultView extends View {
+  _parentEl = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again ;)';
+  _successMessage = '';
+
+  _generateHtml() {
+    return this._data.map(this._generateHtmlPreview).join('');
+  }
+
+  _generateHtmlPreview(result) {
+    return `
+         <li class="preview">
+            <a class="preview__link" href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.image}" alt="${result.title}" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+                
+              </div>
+            </a>
+          </li>
+        `;
+  }
+}
+
+export default new ResultView();
